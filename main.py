b5557dcb56efb0520de07937dccf2c761d9e03ae
Aman Deep <artemisfowl795@gmail.com>
2026-01-17 14:33:33 +0530

    debugging lines removed as the error is resolved

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ DATA_PATH = os.path.join(BASE_DIR, "data", "raw", "laptop_price.csv")
 # Load raw data
 df = pd.read_csv(DATA_PATH)
 
-print("Before cleaning:", df.columns)
 
 from src.data_cleaning import clean_data
 df_clean=clean_data(df)
-print("After cleaning:", df_clean.columns)
+
 
 from src.feature_engineering import engineer_features
 df_fe = engineer_features(df_clean)
-print("After FE:", df_fe.columns)
 
 from src.encoding import prepare_data
 X_train, X_test, y_train, y_test = prepare_data(df_fe)
+
 
 print(df_fe.shape)
 print(df_clean.shape)
